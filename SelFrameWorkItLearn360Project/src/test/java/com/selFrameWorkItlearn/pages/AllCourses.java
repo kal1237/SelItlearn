c8e1e8142c6112b1b227954ef52b9e3259fcf711
kalya <kalya@LAPTOP-LRNS79PC>
2022-06-08 11:54:10 -0500

    including all courses

--- SelFrameWorkItLearn360Project/src/test/java/com/selFrameWorkItlearn/pages/AllCourses.java
+++ SelFrameWorkItLearn360Project/src/test/java/com/selFrameWorkItlearn/pages/AllCourses.java
@@ -1,10 +1,52 @@
 package com.selFrameWorkItlearn.pages;
 
-public class AllCourses {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
+public class AllCourses {
+	WebDriver driver;
+	
+	public AllCourses(WebDriver ldriver)
+	{
+		this.driver=ldriver;
+		
+	}
+
+	@FindBy(xpath="//*[@id=\"primary_menu\"]/a") WebElement allCourses;
+	@FindBy(id="menu-course-menu") WebElement navigationElement;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-23203\"]/a/div") WebElement navigationElement1;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-34626\"]/a/div/span") WebElement popularCourses;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-7397\"]/a/div/span") WebElement softwareTestAcademy;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-12355\"]/a/div/span") WebElement automationTestingwithTestNG;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-12360\"]/a/div/span")WebElement automationWithSeleniumWebDriver;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-23203\"]/a/div/span") WebElement jiraforSoftwareTesters;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-30109\"]/a/div/span")WebElement seleniumWebDriverwithPython;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-23157\"]/a/div/span")WebElement qAManualTester;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-21621\"]/a/div/span") WebElement javaforAutomationTesting;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-12348\"]/a/div/span") WebElement jmeterforPerformanceTestingAdvanced;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-12349\"]/a/div/span") WebElement SoapUIWebServicesTesting;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-19329\"]/a/div/span") WebElement apache_Maven_with_Eclipse;
+	@FindBy(xpath="//*[@id=\"nav-menu-item-12352\"]/a/div/span") WebElement automation_Testing_with_Cucumber;
+	
+	
+	
 	public void allCourses()
 	{
-	
-  System.out.println("All courses");
+		 System.out.println("All courses");
+		 allCourses.click();
+		 Actions action=new Actions(driver);
+		 action.moveToElement(navigationElement).build().perform();
+		 softwareTestAcademy.click();
+		 System.out.println("softwareTestAcademy is clicked");
+		 Assert.assertEquals("POPULAR COURSES", popularCourses.getText());
+		 System.out.println("Expected title is correct");
+		 action.moveToElement(navigationElement1).build().perform();
+		 automationTestingwithTestNG.click();
+		 
+		 
+		 
 }
 }
--- SelFrameWorkItLearn360Project/src/test/java/com/selFrameWorkItlearn/testCases/TestItLearnTestCases.java
+++ SelFrameWorkItLearn360Project/src/test/java/com/selFrameWorkItlearn/testCases/TestItLearnTestCases.java
@@ -3,11 +3,12 @@ package com.selFrameWorkItlearn.testCases;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+import com.selFrameWorkItlearn.pages.AllCourses;
 import com.selFrameWorkItlearn.pages.BaseClassItLearn;
 import com.selFrameWorkItlearn.pages.LoginPage;
 
 public class TestItLearnTestCases extends BaseClassItLearn{
-	
+
 	@Test(priority=1)
 	public void loginItLearn()
 	{
@@ -16,14 +17,20 @@ public class TestItLearnTestCases extends BaseClassItLearn{
 		logger.info("Starting Application");
 		lp.loginDetails(excel.getStringData("login", 0, 0),excel.getStringData("login", 0, 1));
 		logger.pass("Login was successfull");
-		
+
 	}
 
+	@Test(priority=2)
 	public void allCourses()
 	{
-	System.out.println("All Courses");
+		logger=report.createTest("All courses test case 1");
+		AllCourses ac1=PageFactory.initElements(driver,AllCourses.class);
+		logger.info("Starting Application");
+		ac1.allCourses();
+		logger.pass("TestCase1 was successfull");
+		System.out.println("All Courses");
 	}
-	
+
 	public void softwareTestingAcademy()
 	{
 		System.out.println("clicking on Software testing academy");
